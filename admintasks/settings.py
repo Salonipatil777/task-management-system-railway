fe2fdbfccb49ab37b3eeb5794a4763d8e9e0a475
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-07-14 14:06:15 +0530

    Update settings.py

--- admintasks/settings.py
+++ admintasks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^4&%2_j!5+ypa_3hf^%5yma5jpu!l-40+o^f^8cvc52cumt8n7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-b449.up.railway.app','127.0.0:1']
 
 
 # Application definition
