b6bcc4f286315c4b5bb5496f372fc9e1c1167b98
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-07-15 13:38:34 +0530

    Update settings.py

--- admintasks/settings.py
+++ admintasks/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-^4&%2_j!5+ypa_3hf^%5yma5jpu!l-40+o^f^8cvc52cumt8n7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['task-management-system-railway-production.up.railway.app']
 
 # Application definition
 
