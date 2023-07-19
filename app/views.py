23e77cdced00687458094bbce71dd9dd7c155162
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-07-19 14:18:15 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request,'home.html')
 
+
 @login_required(login_url='emplogin')
 def emplogout(request):
     # if request.method == 'POST':
