0a2ee7082e77bf063b8c1c0c715b4c1e8eedd11e
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-07-25 15:19:12 +0530

    Update views.py

--- customadmin/views.py
+++ customadmin/views.py
@@ -18,26 +18,35 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def admin_login(request):
     try:
-        # if request.user.is_authenticated:
-        #     return redirect('/admin/dashboard/')
         if request.method == 'POST':
-            username=request.POST.get('username')
-            password=request.POST.get('password')
-            user_obj=User.objects.filter(username=username)
-            if not user_obj.exists():
-                messages.info(request,'account not found')
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            # Check if the user with the provided username exists
+            user_query = User.objects.filter(username=username)
+            if not user_query.exists():
+                messages.info(request, 'Account not found')
                 return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-            user_obj=authenticate(username=username, password=password)
-            if user_obj and user_obj.is_superuser:
+
+            # Authenticate the user with the provided username and password
+            user_obj = authenticate(username=username, password=password)
+            if user_obj is not None and user_obj.is_superuser:
                 login(request, user_obj)
-                messages.success(request,'admin login successfully')
+                messages.success(request, 'Admin login successful')
                 return redirect('/admin/dashboard/')
-            messages.info(request,'invalid password')
-            return redirect('/')
-        return render(request,'login.html')
+            
+            messages.info(request, 'Invalid password')
+            return redirect('home')
+
+        # For GET requests, simply render the login page
+        return render(request, 'login.html')
+
     except Exception as e:
         print(e)
-        return render(request,'login.html')
+        # You might want to handle the exception and return an appropriate response here
+
+    # Make sure to return a response in all cases to avoid the "None" error
+    return render(request, 'login.html')
 
         
 @login_required(login_url='admin_login')
